9c163d3f39a68345fa823699c13d2fbd02dd0faf
aprhyvs <173691141+aprhyvs@users.noreply.github.com.>
2025-04-30 19:06:52 +0800

    improve last answer to handle following test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,10 @@
-let string = ""
+let result = ""
 
-const repeatString = function() {
-    for (let i = 0; i < 3; i++){
-        string += "hey"
-    } return string
+//expect(repeatString        ('hey', 3)).toEqual('heyheyhey');
+const repeatString = function(string, num) {
+    for (let i = 0; i < num; i++){
+        result += string
+    } return result
 };
 
 // Do not edit below this line
