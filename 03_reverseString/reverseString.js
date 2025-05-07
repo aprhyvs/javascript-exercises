3155882b202f173df1ed516dfdeaed00f2f8003b
aprhyvs <173691141+aprhyvs@users.noreply.github.com.>
2025-05-07 22:49:26 +0800

    solve 03_reverseString
    will be double checked for next commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const arrayString = string.split("").reverse().join("");
+    console.log(arrayString)
+
+    return arrayString
 };
 
 // Do not edit below this line
